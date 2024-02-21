cdb7c04e180a81c383528db1098f542a8cd6e283
martinssluis <luishenrique.martinslima@gmail.com>
2024-02-21 19:38:05 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/projetotriangulo/ProjetoTriangulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetotriangulo;
+
+/**
+ *
+ * @author Dell
+ */
+public class ProjetoTriangulo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
